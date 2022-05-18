d714cfccb9b61255209bec6d0fa584f1834c6e36
YazanAlhalabi <yazanalhalabi1@gmail.com>
2022-05-18 15:11:55 +0300

    remove the code

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,17 +1,5 @@
-import { Request, Response, NextFunction } from 'express'
+function errorHandler404() {}
 
-function errorHandler404(req: Request, res: Response, next: NextFunction) {
-  res.status(404).json({
-    status: 404,
-    message: 'Page Not Found',
-  })
-}
-
-function globalErrorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
-  res.status(400).json({
-    status: 400,
-    message: 'Something wrong',
-  })
-}
+function globalErrorHandler() {}
 
 export default { globalErrorHandler, errorHandler404 }
--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -1,19 +1,3 @@
 import { NextFunction, Request, Response } from 'express'
-import fs from 'fs'
 
-export default function myLogger(req: Request, res: Response, next: NextFunction) {
-  const filePath = './src/logs/requests.txt'
-  const currentDate = new Date()
-  const date = currentDate.toLocaleDateString()
-  const time = currentDate.toLocaleTimeString()
-
-  const dataToWrite = `Method: ${req.method}, Path: ${req.path}, Date: ${date}, Time: ${time}\n`
-
-  fs.appendFile(filePath, dataToWrite, (err) => {
-    if (err) {
-      return next(new Error('FAILED TO LOG REQUEST INFO'))
-    }
-  })
-
-  next()
-}
+export default function myLogger(req: Request, res: Response, next: NextFunction) {}
