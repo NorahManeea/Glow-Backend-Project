2d8f2a82dd52bba5b592c11c81db1a2d82ae9cad
Mehaf <93731842+MehafAllazzam@users.noreply.github.com>
2023-11-19 23:34:58 +0300

    feat/ creati ng Order Model

--- src/models/order.ts
+++ src/models/order.ts
@@ -5,15 +5,36 @@ export type OrderDocument = Document & {
   products: mongoose.Schema.Types.ObjectId[]
 }
 
-const orderSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+    },
+    orderDate: {
+      type: Date,
+      default: Date.now,
+    },
+    products: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: 'Product',
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+    shippingInfo: {
+      country: { type: String, required: true },
+      city: { type: String, required: true },
+      address: { type: String, required: true },
+    },
+    orderStatus: {
+      type: String,
+      enum: ['Processing', 'Shipped', 'Received'],
+      required: true,
+    },
   },
-  products: {
-    type: [mongoose.Schema.Types.ObjectId],
-    ref: 'Product',
-  },
-})
+  { timestamps: true }
+)
 
 export default mongoose.model<OrderDocument>('Order', orderSchema)
