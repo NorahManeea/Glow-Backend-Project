2ec16a14b913c938d75f74a132dea5aeb27413d9
Norah Almaneea <nouraalmanea0@gmail.com>
2023-11-20 21:48:59 +0300

    fix: Implemented user login functionality with password verification

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -12,13 +12,11 @@ import bcrypt from 'bcrypt'
 export const registerUser = async (req: Request, res: Response) => {
   try {
     const { email, firstName, lastName, password } = req.body
-
     // Check if email already registered
     let user = await User.findOne({ email })
     if (user) {
       return res.status(400).json({ message: 'This email is already registered' })
     }
-
     // Hash Pasword
     const salt = await bcrypt.genSalt(10)
     const hashPassword = await bcrypt.hash(req.body.password, salt)
@@ -53,5 +51,14 @@ export const loginUser = async (req: Request, res: Response) => {
   if (!user) {
     return res.status(404).json({ message: 'User not found' })
   }
-  return res.status(200).json({ message: 'Logged In successfully', user })
+  const isPasswordMatch = await bcrypt.compare(req.body.password, user.password)
+  if (!isPasswordMatch) {
+    return res.status(400).json({ message: 'The email or password you entered is invalid' })
+  }
+  res.status(200).json({
+    _id: user._id,
+    email: user.email,
+    firstName: user.firstName,
+    lastName: user.lastName,
+  })
 }
