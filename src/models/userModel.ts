7a9e60cda86670e5442a6349c19025e0466b16f1
Norah Almaneea <nouraalmanea0@gmail.com>
2023-11-20 13:13:08 +0300

    fix: user model

---
+++ src/models/userModel.ts
@@ -0,0 +1,45 @@
+import mongoose, { Document } from 'mongoose'
+
+export type UserDocument = Document & {
+  firstName: string
+  lastName: string
+  email: string
+  password: string
+  role: string
+}
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 30,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 30,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      maxlength: 100,
+    },
+    role: {
+      type: String,
+      enum: ['User', 'Admin'],
+      default: 'User',
+    },
+  },
+  { timestamps: true }
+)
+
+export const User = mongoose.model<UserDocument>('User', userSchema)
