a85215f534be801f7582c65e12e714f51c23d449
Norah Almaneea <nouraalmanea0@gmail.com>
2023-11-23 15:45:56 +0300

    userValidate using Zod

--- src/middlewares/validateUser.ts
+++ src/middlewares/validateUser.ts
@@ -1,10 +1,34 @@
-import { NextFunction, Request, Response } from "express";
-import zod from 'zod'
-import ApiError from "../errors/ApiError";
+import { NextFunction, Request, Response } from 'express'
+import zod, { ZodError } from 'zod'
+import ApiError from '../errors/ApiError'
 
-export function validateUser (req: Request, res: Response, next: NextFunction){
-    const schema = {
-        email: zod.string().email(),
-        password: zod.string().min(8)
+export function validateUser(req: Request, res: Response, next: NextFunction) {
+  const schema = zod.object({
+    firstName: zod
+      .string()
+      .min(3, { message: 'First name is required' })
+      .max(30, { message: 'First name must be 30 characters or less' }),
+    lastName: zod
+      .string()
+      .min(3, { message: 'Last name is required' })
+      .max(30, { message: 'Last name must be 30 characters or less' }),
+    email: zod.string().email({ message: 'Invalid email address' }),
+    password: zod
+      .string()
+      .min(8, { message: 'Password must be at least 8 characters' })
+      .max(100, { message: 'Password must be 100 characters or less' }),
+  })
+
+  try {
+    schema.parse(req.body)
+    next()
+  } catch (error) {
+    const err = error
+    if (err instanceof ZodError) {
+      next(ApiError.badRequestValidation(err.errors))
+      return
     }
+
+    next(ApiError.internal('Something went wrong'))
+  }
 }
--- src/routers/authRoute.ts
+++ src/routers/authRoute.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 import { createrUser, loginUser, registerUser } from '../controllers/authController'
+import { validateUser } from '../middlewares/validateUser';
 
 const router = express.Router()
 
-router.post("/register", registerUser);
+router.post("/register",validateUser, registerUser);
 router.post("/login", loginUser);
 router.get("/activate/:token",createrUser)
 
