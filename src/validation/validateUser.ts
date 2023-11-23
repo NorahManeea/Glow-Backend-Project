d5c49df86e77a2babcb1277b4382a8b9f0c04e7d
Norah Almaneea <nouraalmanea0@gmail.com>
2023-11-23 16:12:07 +0300

    feat: validate product

--- src/validation/validateUser.ts
+++ src/validation/validateUser.ts
@@ -4,19 +4,7 @@ import ApiError from '../errors/ApiError'
 
 export function validateUser(req: Request, res: Response, next: NextFunction) {
   const schema = zod.object({
-    firstName: zod
-      .string()
-      .min(3, { message: 'First name is required' })
-      .max(30, { message: 'First name must be 30 characters or less' }),
-    lastName: zod
-      .string()
-      .min(3, { message: 'Last name is required' })
-      .max(30, { message: 'Last name must be 30 characters or less' }),
-    email: zod.string().email({ message: 'Invalid email address' }),
-    password: zod
-      .string()
-      .min(8, { message: 'Password must be at least 8 characters' })
-      .max(100, { message: 'Password must be 100 characters or less' }),
+   
   })
 
   try {
