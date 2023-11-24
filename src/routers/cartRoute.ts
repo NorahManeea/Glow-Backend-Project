a929c3474ad3ea61523733af5f5e34794c94d807
Norah Almaneea <nouraalmanea0@gmail.com>
2023-11-25 01:17:19 +0300

    fix

--- src/routers/cartRoute.ts
+++ src/routers/cartRoute.ts
@@ -6,11 +6,13 @@ import {
   deleteCartItem,
 } from '../controllers/cartController'
 import { validateObjectId } from '../middlewares/validateObjectId'
+import { validateCart } from '../validation/validateCart'
+
 
 const router = express.Router()
 
-router.post('/', addToCart)
-router.put('/:id', validateObjectId, updateCartItems)
+router.post('/', validateCart,addToCart)
+router.put('/:id', validateObjectId,validateCart, updateCartItems)
 router.get('/:id', validateObjectId, getCartItems)
 router.delete('/:id', deleteCartItem)
 
