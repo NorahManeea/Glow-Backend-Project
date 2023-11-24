cfe20f52adc272fbd31f35d4b69795262becaff0
Norah Almaneea <nouraalmanea0@gmail.com>
2023-11-24 17:44:05 +0300

    feat: Produt services

---
+++ src/database/db.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+import { dbConfig } from '../config/db.config'
+
+const URL = dbConfig.db.url
+
+export const databseConnection = ()=> {
+    mongoose
+  .connect(URL)
+  .then(() => {
+    console.log('Database connected')
+  })
+  .catch((err) => {
+    console.log('MongoDB connection error, ', err)
+  })
+}
