583a1bab031c06dc771d9a5cf5ac5b3e855a122c
Norah Almaneea <nouraalmanea0@gmail.com>
2023-11-24 19:22:02 +0300

    feat: Category Services

--- src/controllers/categoryController.ts
+++ src/controllers/categoryController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 import { Category } from '../models/categoryModel'
 import ApiError from '../errors/ApiError'
+import { createNewCategory, findAllCategories, findCategory, removeCategory, updateCategory } from '../services/categoryService'
 
 /**-----------------------------------------------
  * @desc Create Category 
@@ -10,14 +11,9 @@ import ApiError from '../errors/ApiError'
  -----------------------------------------------*/
 export const createCategory = async (req: Request, res: Response) => {
   try {
-    const { categoryName } = req.body
-    const category = await Category.create({
-      categoryName: categoryName,
-    })
-    await category.save()
-    res.status(201).json(category)
+    const category = await createNewCategory(req.body)
+    res.status(201).json({ meassge: 'Category has been created successfuly', payload: category })
   } catch (error) {
-    console.log(error)
     res.status(500).json({ message: error })
   }
 }
@@ -28,8 +24,14 @@ export const createCategory = async (req: Request, res: Response) => {
  * @access public 
  -----------------------------------------------*/
 export const getAllCategory = async (req: Request, res: Response) => {
-  const categories = await Category.find()
-  res.status(200).json(categories)
+  try {
+    const categories = await findAllCategories()
+    res
+    .status(200)
+    .json({ message: 'All categories returned successfully', payload: categories })
+  } catch (error) {
+    res.status(500).json({ message: error })
+  }
 }
 /**-----------------------------------------------
  * @desc Get Category by Id
@@ -38,9 +40,13 @@ export const getAllCategory = async (req: Request, res: Response) => {
  * @access public
  -----------------------------------------------*/
 export const getCategoryById = async (req: Request, res: Response) => {
-  const categoryId = req.params.id
-  const category = await Category.findById(categoryId)
-  res.status(200).json(category)
+ try {
+  const categories = await findCategory(req.params.id)
+  res.status(200)
+  .json({ message: 'Single category returned successfully', payload: categories })
+ } catch (error) {
+  res.status(500).json({ message: error })
+ }
 }
 /**-----------------------------------------------
  * @desc Update Category 
@@ -48,26 +54,13 @@ export const getCategoryById = async (req: Request, res: Response) => {
  * @method PUT
  * @access private (Admin Only)
  -----------------------------------------------*/
-export const updateCategory = async (req: Request, res: Response) => {
+export const updateCategoryById = async (req: Request, res: Response) => {
   try {
-    const newCategoryName = req.body.categoryName
-    const categoryId = await Category.findById(req.params.id)
-    if (!categoryId) {
-      res.status(404).json({ message: 'Category Not Found' })
-    }
-    const updatedCategory = await Category.findByIdAndUpdate(
-      categoryId,
-      {
-        $set: {
-          categoryName: newCategoryName,
-        },
-      },
-      { new: true }
-    )
-    res.status(200).json(updatedCategory)
+    const categoryId = req.params.id
+    const updatedCategory = await updateCategory(categoryId, req.body)
+    res.status(200).json({message: 'Category has been updated successfully', payload: updatedCategory})
   } catch (error) {
-    console.error(error)
-    res.status(500).json({ message: 'Internal Server Error' })
+    res.status(500).json({ message: error })
   }
 }
 /**-----------------------------------------------
@@ -77,17 +70,10 @@ export const updateCategory = async (req: Request, res: Response) => {
  * @access private (Admin Only)
  -----------------------------------------------*/
 export const deleteCategory = async (req: Request, res: Response) => {
-  const categoryId = await Category.findById(req.params.id)
   try {
-    if (!categoryId) {
-      return res.status(404).json({ message: 'Category not found' })
-    }
-    await Category.deleteOne({
-      _id: categoryId,
-    })
-    res.status(200).json({ meassge: 'Category deleted successfuly', result: categoryId })
+  const category = await removeCategory(req.params.id)
+    res.status(200).json({ meassge: 'Category has been deleted Successfully', result: category })
   } catch (error) {
-    console.log(error)
     res.status(500).json({ message: error })
   }
 }
