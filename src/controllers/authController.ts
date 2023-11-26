d97514bf38f3bc06a6048e4cd103af94a1315e09
Norah Almaneea <nouraalmanea0@gmail.com>
2023-11-26 23:51:56 +0300

    fix: verify token

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,11 +1,11 @@
-import { Request, Response } from 'express'
-import { User } from '../models/userModel'
-
-import jwt, { JwtPayload, TokenExpiredError } from 'jsonwebtoken'
-
+import { NextFunction, Request, Response } from 'express'
+import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
+
+import ApiError from '../errors/ApiError'
 import { authConfig } from '../config/auth.config'
-import { sendEmail } from '../utils/sendEmail'
+import { generateActivationToken, sendEmail } from '../utils/sendEmail'
+import { User } from '../models/userModel'
 
 /** -----------------------------------------------
  * @desc Register User
@@ -13,44 +13,44 @@ import { sendEmail } from '../utils/sendEmail'
  * @method POST
  * @access public
   -----------------------------------------------*/
-export const registerUser = async (req: Request, res: Response) => {
+export const registerUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { email, firstName, lastName, password } = req.body
     let user = await User.findOne({ email })
     if (user) {
       return res.status(400).json({ message: 'This email is already registered' })
     }
-    const salt = await bcrypt.genSalt(10)
-    const hashPassword = await bcrypt.hash(password, salt)
-    const tokenPayload = {
-      email: email,
-      firstName: firstName,
-      lastName: lastName,
+    const token = generateActivationToken()
+    const hashPassword = await bcrypt.hash(password, 10)
+
+    const newUser = new User({
+      email,
+      firstName,
+      lastName,
       password: hashPassword,
-    }
-    const token = jwt.sign(tokenPayload, authConfig.jwt.accessToken, {expiresIn: '10m'})
+      token,
+      role: 'USER',
+    })
+
+    await newUser.save()
+    const activationLink = `http://localhost:5050/api/auth/activate/${token}`
 
     const subject = 'Welcome to Black Tigers Team!'
     const htmlTemplate = `
-    <div style="color: #333; text-align: center;">
-      <h1 style="color: #1E1E1E;">Welcome to Black Tigers Team!</h1>
-      <a href="http://localhost:5050/api/auth/activate/${token}" style="display: inline-block; padding: 10px 20px; background-color: #664971; color: #FFFFFF; font-size: 18px; text-decoration: none; border-radius: 5px;">Activate Now</a>
-      <p style="font-size: 14px; color: #302B2E;">Black Tigers Team</p>
-    </div>
-    `
+        <div style="color: #333; text-align: center;">
+          <h1 style="color: #1E1E1E;">Welcome to Black Tigers Team!</h1>
+          <a href="${activationLink}" style="display: inline-block; padding: 10px 20px; background-color: #664971; color: #FFFFFF; font-size: 18px; text-decoration: none; border-radius: 5px;">Activate Now</a>
+          <p style="font-size: 14px; color: #302B2E;">Black Tigers Team</p>
+        </div>
+      `
     await sendEmail(email, subject, htmlTemplate)
 
     res.status(201).json({
       message: 'Registration successful. Check your email to activate your account',
-      token: token,
+      newUser,
     })
   } catch (error) {
-    console.error(error)
-    if(error instanceof TokenExpiredError){
-      return res.status(401).json({ error: 'Token has expired' });
-    } else{
-      res.status(500).json({ error: error })
-    }
+    next(ApiError.badRequest('Something went wrong'))
   }
 }
 
@@ -60,28 +60,22 @@ export const registerUser = async (req: Request, res: Response) => {
  * @method GET
  * @access private
   -----------------------------------------------*/
-export const createrUser = async (req: Request, res: Response) => {
+export const activateUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const token = req.params.token
-
-    const verifiedToken = jwt.verify(token, authConfig.jwt.accessToken) as JwtPayload
-
-    if (!verifiedToken) {
-      return res.status(400).json({ message: 'Invalid token' })
+    const activationToken = req.params.token
+    const user = await User.findOneAndUpdate(
+      { token: activationToken },
+      { isAccountVerified: true, token: undefined },
+      { new: true }
+    )
+    if (!user) {
+      next(ApiError.badRequest('Invalid token'))
+      return
     }
-    const { email, firstName, lastName, password } = verifiedToken
-    const user = new User({
-      email: email,
-      firstName: firstName,
-      lastName: lastName,
-      password: password,
-      token
+    res.status(200).json({
+      message: 'Your Account has been activated successfull',
     })
-
-    await user.save()
-    res.status(200).json({ message: 'Your Account has been activated successfully' })
   } catch (error) {
-    console.error(error)
     res.status(500).json({ error: error })
   }
 }
@@ -92,7 +86,6 @@ export const createrUser = async (req: Request, res: Response) => {
  * @method POST
  * @access public
   -----------------------------------------------*/
-
 export const loginUser = async (req: Request, res: Response) => {
   const { email, password } = req.body
   if (!email || !password) {
@@ -107,10 +100,18 @@ export const loginUser = async (req: Request, res: Response) => {
   if (!isPasswordMatch) {
     return res.status(400).json({ message: 'The email or password you entered is invalid' })
   }
-  res.status(200).json({
-    _id: user._id,
-    email: user.email,
-    firstName: user.firstName,
-    lastName: user.lastName,
-  })
+
+  const token = jwt.sign(
+    {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      role: user.role,
+    },
+    authConfig.jwt.accessToken as string,
+    {
+      expiresIn: '10m',
+    }
+  )
+  res.status(200).json({ message: 'Login successful', token })
 }
