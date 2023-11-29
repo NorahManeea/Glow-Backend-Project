326afb93574cb8bf693e69465f652a64c46a80b7
Alhanouf Allazzam <alhanoufkmlz@gmail.com>
2023-11-29 23:26:58 +0300

    fix: clean category route & controller & services

--- src/services/categoryService.ts
+++ src/services/categoryService.ts
@@ -14,15 +14,11 @@ export const findAllCategories = async (pageNumber = 1, limit = 8, searchText = 
   const categories = await Category.find()
     .skip(skip)
     .limit(limit)
-    .find(
-      searchText
-        ? {
-            $or: [
-              { categoryName: { $regex: searchText, $options: 'i' } },
-            ],
-          }
-        : {}
-    )
+    .find(searchText ? { categoryName: { $regex: searchText, $options: 'i' } } : {})
+
+  if (categories.length == 0) {
+    throw ApiError.notFound('There are no categories')
+  }
 
   return { categories, totalPages, currentPage: pageNumber }
 }
@@ -30,40 +26,44 @@ export const findAllCategories = async (pageNumber = 1, limit = 8, searchText = 
 export const findCategory = async (categoryId: string) => {
   const category = await Category.findById(categoryId)
   if (!category) {
-    return ApiError.notFound(`Category not found with the ID: ${categoryId}`)
+    throw ApiError.notFound(`Category not found with ID: ${categoryId}`)
   }
+
   return category
 }
 
 export const updateCategory = async (categoryId: string, updatedCategory: CategoryDocument) => {
-  const { categoryName } = updatedCategory
-  const categoryExist = await Category.exists({ categoryName: categoryName })
+  const categoryExist = await Category.exists({ categoryName: updatedCategory.categoryName })
   if (categoryExist) {
-    throw ApiError.conflict(`Category already exists with this ${categoryName}`)
+    throw ApiError.conflict(`Category already exists with name: ${updatedCategory.categoryName}`)
   }
+
   const category = await Category.findByIdAndUpdate(categoryId, updatedCategory, { new: true })
   if (!category) {
-    return ApiError.notFound(`Category not found with the ID: ${categoryId}`)
+    throw ApiError.notFound(`Category not found with ID: ${categoryId}`)
   }
+
   return category
 }
 
 export const removeCategory = async (categoryId: string) => {
   const category = await Category.findByIdAndDelete(categoryId)
   if (!category) {
-    return ApiError.notFound(`Category not found with the ID: ${categoryId}`)
+    throw ApiError.notFound(`Category not found with ID: ${categoryId}`)
   }
+
   return category
 }
 
 export const createNewCategory = async (newCategory: CategoryDocument) => {
-  const { categoryName } = newCategory
-  const categoryExist = await Category.exists({ categoryName: categoryName })
+  const categoryExist = await Category.exists({ categoryName: newCategory.categoryName })
   if (categoryExist) {
-    return ApiError.conflict(`Category already exists with this ${categoryName}`)
+    throw ApiError.conflict(`Category already exists with name: ${newCategory.categoryName}`)
   }
+
   const category = await Category.create({
     ...newCategory,
   })
+
   return category
 }
