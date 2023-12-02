703ed0927844b9ccb9a72a06ecdb8bae700dfb92
Norah Almaneea <nouraalmanea0@gmail.com>
2023-12-02 23:17:21 +0300

    refactoring

--- src/controllers/reviewController.ts
+++ src/controllers/reviewController.ts
@@ -3,6 +3,7 @@ import { createNewReview, findAllReviews, removeReview } from '../services/revie
 import { Review } from '../models/reviewModel'
 import { findProduct } from '../services/productService'
 import ApiError from '../errors/ApiError'
+import { findOrderHistory } from '../services/orderService'
 
 /** -----------------------------------------------
  * @desc Get All Reviews
@@ -23,29 +24,37 @@ export const getAllReviews = async (req: Request, res: Response, next: NextFunct
  * @desc Add Review
  * @route /api/reviews/
  * @method POST
- * @access private (logged in user)
+ * @access private (only registered users)
   -----------------------------------------------*/
 export const addNewReview = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { productId, reviewText } = req.body
-    await findProduct(productId)
+    // Check if the user has ordered the product
+    const orderHistory = await findOrderHistory(req.decodedUser.userId)
+    const orderedProductIds = orderHistory.map((order) =>
+      order.products.map((product) => product.product.toString())
+    )
+    if (!orderedProductIds.includes(productId)) {
+      return next(ApiError.badRequest('You can only review products that you have ordered.'))
+    }
+
     const review = new Review({
       userId: req.decodedUser.userId,
       productId,
       reviewText,
     })
+
     await createNewReview(review)
     res.status(201).json({ message: 'Review added successfully', payload: review })
   } catch (error) {
     next(error)
   }
 }
-
 /** -----------------------------------------------
  * @desc Delete Review
  * @route /api/reviews/:reviewId
  * @method DELETE
- * @access private (Admin or logged in user)
+ * @access private (Admin or user who wrote the review)
   -----------------------------------------------*/
 export const deleteReview = async (req: Request, res: Response, next: NextFunction) => {
   try {
